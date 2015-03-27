65ee655e162d491f5bf0f4698943b57006e3478e
bnw <s6beklot@uni-bonn.de>
2015-03-27 20:51:09 +0100

    test that enumartion can be aborted

--- tests/algorithms/enumerateBStarTrees/EnumeratorTest.cpp
+++ tests/algorithms/enumerateBStarTrees/EnumeratorTest.cpp
@@ -8,7 +8,6 @@
 
 namespace rechteckpackungen {
 
-
 void testEnumerator() {
 	auto enumerator = enumerateBStarTrees::Enumerator();
 	auto a = std::shared_ptr<Rectangle>(new Rectangle(1, 1));
@@ -18,12 +17,21 @@ void testEnumerator() {
 	rectangles->push_back(b);
 
 	int count = 0;
-
-	enumerator.forEachBStarTree(rectangles, [&](BStarTree* tree){
+	enumerator.forEachBStarTree(rectangles, [&](BStarTree* tree) {
 		count++;
+		return true;
 	});
-
 	ASSERT_EQUAL(4, count);
+
+	count = 0;
+	enumerator.forEachBStarTree(rectangles, [&](BStarTree* tree) {
+		count++;
+		if(count == 2) {
+			return false;
+		}
+		return true;
+	});
+	ASSERT_EQUALM("Assert that iteration can be aborted", 2, count);
 }
 
 cute::suite make_suite_EnumeratorTest() {
