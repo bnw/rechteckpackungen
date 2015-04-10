b4a4b47f64d2550b00dac7ccc2d2febe39b73839
bnw <s6beklot@uni-bonn.de>
2015-04-10 16:01:31 +0200

    refactored

--- src/algorithms/enumerateSubsets/Enumerator.cpp
+++ src/algorithms/enumerateSubsets/Enumerator.cpp
@@ -1,10 +1,16 @@
 #include "Enumerator.h"
 
 namespace rechteckpackungen {
+
 namespace enumerateSubsets {
 
-void constructSubset(const std::vector<void*>& set, unsigned setIndex, unsigned subsetIndex, std::vector<void*>& partialSubset,
-		Enumerator::callbackType callback) {
+void Enumerator::forEachSubset(const std::vector<void*>& set, unsigned sizeOfSubset, Enumerator::callbackType callback) const {
+	auto partialSubset = std::vector<void*>(sizeOfSubset, nullptr);
+	constructSubset(set, 0, 0, partialSubset, callback);
+}
+
+void Enumerator::constructSubset(const std::vector<void*>& set, unsigned setIndex, unsigned subsetIndex, std::vector<void*>& partialSubset,
+		Enumerator::callbackType callback) const{
 	auto sizeOfSubset = partialSubset.size();
 	if (subsetIndex == sizeOfSubset) {
 		callback(partialSubset);
@@ -14,11 +20,6 @@ void constructSubset(const std::vector<void*>& set, unsigned setIndex, unsigned 
 		partialSubset.at(subsetIndex) = set.at(i);
 		constructSubset(set, i + 1, subsetIndex + 1, partialSubset, callback);
 	}
-}
-
-void Enumerator::forEachSubset(const std::vector<void*>& set, unsigned sizeOfSubset, Enumerator::callbackType callback) const {
-	auto partialSubset = std::vector<void*>(sizeOfSubset, nullptr);
-	constructSubset(set, 0, 0, partialSubset, callback);
 }
 
 }
--- src/algorithms/enumerateSubsets/Enumerator.h
+++ src/algorithms/enumerateSubsets/Enumerator.h
@@ -11,6 +11,9 @@ class Enumerator {
 public:
 	typedef const std::function<void(const std::vector<void*>&)>& callbackType;
 	void forEachSubset(const std::vector<void*>& set, unsigned sizeOfSubset, callbackType callback) const;
+protected:
+	void constructSubset(const std::vector<void*>& set, unsigned setIndex, unsigned subsetIndex, std::vector<void*>& partialSubset,
+			Enumerator::callbackType callback) const;
 };
 
 }
