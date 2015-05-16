e4ff72b2a8e1449a98c4975ae347b667b5c79b94
bnw <s6beklot@uni-bonn.de>
2015-05-16 14:21:40 +0200

    added comment

--- src/algorithms/enumerateSubsets/Enumerator.h
+++ src/algorithms/enumerateSubsets/Enumerator.h
@@ -12,6 +12,11 @@ class Enumerator {
 public:
 	typedef std::vector<F> vector;
 	typedef const std::function<void(const vector)> callbackType;
+
+	/*
+	 * Calls callback once for every possible subset of size sizeOfSubset of set.
+	 * Elements in the subset will maintain the order they had in set.
+	 */
 	void forEachSubset(const vector& set, const unsigned sizeOfSubset, callbackType &callback) const;
 protected:
 	void constructSubset(const vector& set, unsigned setIndex, unsigned subsetIndex, vector& partialSubset, callbackType& callback) const;
