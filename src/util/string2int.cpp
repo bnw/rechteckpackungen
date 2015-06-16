1c45acab1dfd693282aa406586422bc058a72836
bnw <s6beklot@uni-bonn.de>
2015-06-16 19:00:37 +0200

    Added exception, when trying to convert empty string to int

--- src/util/string2int.cpp
+++ src/util/string2int.cpp
@@ -3,6 +3,9 @@
 namespace rechteckpackungen {
 
 int string2int(std::string string) {
+	if(string.length() == 0){
+		throw std::runtime_error("Could not convert empty string to int");
+	}
 	for (auto k = string.begin(); k != string.end(); ++k) {
 		if (!isdigit(*k)) {
 			throw std::runtime_error("Could not convert string to int");
--- tests/util/String2intTest.cpp
+++ tests/util/String2intTest.cpp
@@ -9,6 +9,7 @@ void testString2int() {
 	ASSERT(string2int("0") == 0);
 	ASSERT(string2int("55") == 55);
 	ASSERT_THROWS(string2int("55a"), std::runtime_error);
+	ASSERT_THROWS(string2int(""), std::runtime_error);
 }
 
 cute::suite make_suite_String2intTest() {
